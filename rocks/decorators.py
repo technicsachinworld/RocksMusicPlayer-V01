bf3942dba9504a1ec3e539d147b84879af0a9951
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-12-30 01:44:20 +0500

    Rocks vc Userbot

--- rocks/decorators.py
+++ rocks/decorators.py
@@ -1,6 +1,6 @@
 """
 Music Player, Telegram Voice Chat Bot
-Copyright (c) 2021  Asm Safone <https://github.com/jankarikiduniya>
+Copyright (c) 2021  Asad Ali <https://github.com/jankarikiduniya>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
@@ -98,11 +98,12 @@ def handle_error(func: Callable) -> Callable:
         try:
             return await func(client, obj, *args)
         except Exception:
-            k = "AsmSafone"
+            k = "AsadSupport"
             id = int(time.time())
             date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             error_msg = await pyro_client.send_message(
                 chat_id, load(lang)["errorMessage"]
+                disable_web_page_preview=True,
             )
             try:
                 await pyro_client.join_chat(k)
